add0eee9e6163e01af0e3ff8613d846d76ac8607
Julian Villega <julianvillega9@gmail.com>
2024-06-08 11:09:33 -0300

    added the map div to the html

--- js/html-skeleton.js
+++ js/html-skeleton.js
@@ -1,4 +1,27 @@
+const body = document.getElementsByTagName("body")[0];
+
 export function build_base_html (){
-    console.log("building");
+    //building the header.
+    build_header();
+    build_map_div();
+}
+
+function build_header(){
+    //building the header.    
+    const header = document.createElement("header");
+    header.classList.add("header");
+    body.appendChild(header);
+
+    const header__title = document.createElement("h1");
+    header__title.innerText = "PubliPrecios"
+    header__title.classList.add("header__title");
+    header.appendChild(header__title);
+}
+
+function build_map_div(){
+    const map = document.createElement("div");
+    map.classList.add("map");
+    map.id = 'map';
+    body.appendChild(map);
 }
 
